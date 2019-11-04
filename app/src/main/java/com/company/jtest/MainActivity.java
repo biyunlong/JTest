6e4da706adbfef3cdfbfa220233bc4fa253b8f58
eb <1225198434@qq.com>
2019-11-04 17:43:01 +0800

    first commit

---
+++ app/src/main/java/com/company/jtest/MainActivity.java
@@ -0,0 +1,20 @@
+package com.company.jtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.gson.Gson;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        new Gson();
+    }
+}
